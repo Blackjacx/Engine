754286c22a3424886e7828f3f8b9c3baddfb5bfa
Stefan Herold <stefan.herold@gmail.com>
2021-07-05 01:12:39 +0200

    Remove deprecated request function

--- Sources/Engine/Networking/Network.swift
+++ Sources/Engine/Networking/Network.swift
@@ -26,7 +26,6 @@ public struct Network {
 
     // MARK: - Async / Await
 
-    /// - throws: NetworkError
     public func request<T: Decodable>(endpoint: Endpoint) async throws -> T {
 
         let url = endpoint.buildUrl()
@@ -68,104 +67,16 @@ public struct Network {
             throw NetworkError.invalidStatusCode(code: response.statusCode, underlying: error)
         }
 
-        do {
-            if Self.verbosityLevel > 0 {
-                let json = String(data: data, encoding: .utf8)!
-                print(json)
-            }
-
-            // Prevents decoding error when there is no data in the response
-            if T.self == EmptyResponse.self {
-              return EmptyResponse() as! T
-            }
-
-            return try endpoint.jsonDecode(T.self, from: data)
-        } catch {
-            throw NetworkError.jsonDecodingFailed(error: error)
-        }
-    }
-
-    // MARK: - Deprecated
-
-    public func request<T: Decodable>(endpoint: Endpoint, completion: @escaping RequestClosure<T>) {
-
-        let url = endpoint.buildUrl()
-
-        var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 5)
-        request.httpMethod = endpoint.method.rawValue
-        request.allHTTPHeaderFields = endpoint.headers
-
-        if Self.verbosityLevel > 1 {
-            print("Request: [\(endpoint.method.rawValue)] \(url) • Headers: [")
-            endpoint.headers?.forEach { print("\t \($0)") }
-            print("]")
+        if Self.verbosityLevel > 0 {
+            let json = String(data: data, encoding: .utf8)!
+            print(json)
         }
 
-        if let parameters = endpoint.parameters {
-            do {
-                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
-            } catch {
-                completion(.failure(.parameterEncodingToJsonFailed(error: error)))
-                return
-            }
+        // Prevents decoding error when there is no data in the response
+        if T.self == EmptyResponse.self {
+          return EmptyResponse() as! T
         }
 
-        Self.session.dataTask(with: request) { (data, response, error) in
-
-            guard let response = response as? HTTPURLResponse else {
-                completion(.failure(.invalidResponse(error: error)))
-                return
-            }
-
-            if Self.verbosityLevel > 1 {
-                print("\nResponse: \(response)")
-            }
-
-            guard (200..<400).contains(response.statusCode) else {
-
-                var loggedError: Swift.Error? = error
-                if let data = data, let failureMessage = String(data: data, encoding: .utf8) {
-                    loggedError = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: failureMessage])
-                }
-                completion(.failure(.invalidStatusCode(code: response.statusCode, underlying: loggedError)))
-                return
-            }
-
-            guard let data = data else {
-                completion(.failure(.noData(error: error)))
-                return
-            }
-
-            do {
-                if Self.verbosityLevel > 0 {
-                    let json = String(data: data, encoding: .utf8)!
-                    print(json)
-                }
-
-                // Prevents decoding error when there is no data in the response
-                if T.self == EmptyResponse.self {
-                  completion(.success(EmptyResponse() as! T))
-                  return
-                }
-
-                let decodable = try endpoint.jsonDecode(T.self, from: data)
-                completion(.success(decodable))
-            } catch {
-                completion(.failure(.jsonDecodingFailed(error: error)))
-            }
-        }.resume()
-    }
-
-    public func syncRequest<T: Decodable>(endpoint: Endpoint) -> RequestResult<T> {
-
-        let semaphore = DispatchSemaphore(value: 0)
-        var result: RequestResult<T>!
-
-        request(endpoint: endpoint) { (receivedResult: RequestResult<T>) in
-            result = receivedResult
-            semaphore.signal()
-        }
-        semaphore.wait()
-        return result
+        return try endpoint.jsonDecode(T.self, from: data)
     }
 }
